785560422375e6cca3193eba94447bb5bde7c358
work2win <worknpeace2win@gmail.com>
2024-08-22 16:49:40 -0400

    updation on class

--- src/main/java/com/work2win/kafka/kafkaproducer/MessageProducer.java
+++ src/main/java/com/work2win/kafka/kafkaproducer/MessageProducer.java
@@ -1,4 +1,8 @@
 package com.work2win.kafka.kafkaproducer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -6,6 +10,7 @@ import java.util.logging.Logger;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.ProducerConfig;
+
 import org.apache.kafka.common.serialization.StringSerializer;
 
 
@@ -15,15 +20,15 @@ public class MessageProducer {
 	
 	public static void main( String[] args )
     {
-        
+		String topicName = "events";
         Properties properties = new Properties();
-     
+       
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer(properties);
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("sampleTopicName","messageproducer1");
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("sampleTopicName","Hello How are you too?");
         
         
         try {
@@ -33,11 +38,11 @@ public class MessageProducer {
 			
 			e.printStackTrace();
 			e.getStackTrace();
-		}
-       
-        kafkaProducer.flush();
-        kafkaProducer.close();
+		}    
+    
         
+        kafkaProducer.flush();
+        kafkaProducer.close();        
         
     }
 
